9fe3c53fafcd2e6f5694fa245f63a60757844c97
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2021-06-22 20:04:35 +0900

    log4J

---
+++ WorkBook/src/main/java/com/WorkBook/CreateXSSFWorkBook.java
@@ -0,0 +1,12 @@
+package com.WorkBook;
+
+import org.apache.log4j.Logger;
+
+public class CreateXSSFWorkBook {
+	private static final Logger LOG = Logger.getLogger(CreateXSSFWorkBook.class);
+	public static void main(String[] args) {
+		LOG.info("CreateXSSFWorkBook");
+
+	}
+
+}
